0e160201950ac9a8c64999660e19c8cb925403e6
Tabitha Philistin Destin <104877297+Shabinejosias@users.noreply.github.com>
2022-06-26 14:50:45 -0300

    Subindo meu exercício aula 3 do javasript

---
+++ script.js
@@ -0,0 +1,26 @@
+function myFunction(){
+    var nome = prompt("Digite seu nome: ");
+    console.log("Nome digitado:" + nome);
+    console.log(typeof(nome));
+    var sobrenome = prompt("Digitado seu sobrenome");
+    console.log("sobrenome digitado:" +sobrenome);
+    console.log(typeof(sobrenome));
+    var idade = prompt("Digita sua idade");
+    console.log("Idade digitado:" + idade);
+    console.log(typeof(idade));
+    var nacionalidade = prompt("Ditige sua nacionalidade:")
+    console.log("Nationalidade digitado:" + nacionalidade);
+    console.log(typeof(nacionalidade));
+    var cor = prompt("Digite sua cor: ");
+    console.log("cor digitado:" + nacionalidade);
+    console.log(typeof(nacionalidade));
+    var sexo = prompt("Digite seu sexso: ");
+    console.log("Sexo digitado: " + nacionalidade);
+    console.log(typeof(sexo));
+    var profissao = prompt("Digite sua profissao");
+    console.log("Profissao digitado: " + profissao);
+    console.log(typeof(profissao));  
+}
+
+
+
