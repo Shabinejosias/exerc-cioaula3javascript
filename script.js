1849c12002455a51502a302e84822210efb0fb94
Tabitha Philistin Destin <104877297+Shabinejosias@users.noreply.github.com>
2022-06-26 18:55:14 -0300

    Atualizando Exercício Aula 3

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-function myFunction(){
+function myFunction() {
     var nome = prompt("Digite seu nome: ");
     console.log("Nome digitado:" + nome);
-    console.log(typeof(nome));
+
     var sobrenome = prompt("Digitado seu sobrenome");
-    console.log("sobrenome digitado:" +sobrenome);
-    console.log(typeof(sobrenome));
+    console.log("sobrenome digitado:" + sobrenome);
+
     var idade = prompt("Digita sua idade");
     console.log("Idade digitado:" + idade);
-    console.log(typeof(idade));
-    var nacionalidade = prompt("Ditige sua nacionalidade:")
-    console.log("Nationalidade digitado:" + nacionalidade);
-    console.log(typeof(nacionalidade));
+
+    var nacionalidade = prompt("Ditige sua nacionalidade: ");
+    console.log("Nationalidade digitado: " + nacionalidade);
+
     var cor = prompt("Digite sua cor: ");
     console.log("cor digitado:" + nacionalidade);
-    console.log(typeof(nacionalidade));
+
     var sexo = prompt("Digite seu sexso: ");
     console.log("Sexo digitado: " + nacionalidade);
-    console.log(typeof(sexo));
+
     var profissao = prompt("Digite sua profissao");
     console.log("Profissao digitado: " + profissao);
-    console.log(typeof(profissao));  
+    console.table([nome, sobrenome, idade, nationalidade, cor, sexo, profissao]);
+
 }
 
 
